4e21769629522f19c57f4ecb80e7ae7e57b23703
davronoff <ilyosbekdavronov009@gmail.com>
2024-08-09 09:55:41 +0500

    test_role.py get-user

--- tests/test_users.py
+++ tests/test_users.py
@@ -20,3 +20,16 @@ def test_get_all_users_authenticated(test_user, test_role):
                                'phone_number': '992226205', 'username': 'davronoff', 'name':
                                 'Davronov Ilyas', 'created_at': data, 'is_active': True, 'user_photo': 'no photo yet',
                                 'role_id': test_role.id}]
+
+
+def test_get_user_authenticated(test_user, test_role):
+    response = client.get("/users/get-user")
+    data = test_user.created_at.isoformat()
+    print(response.json())
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()['id'] == 1
+    assert response.json()['email'] == 'davronof123@gmail.com'
+    assert response.json()['phone_number'] == '992226205'
+    assert response.json()['username'] == 'davronoff'
+    assert response.json()['name'] == 'Davronov Ilyas'
+    assert response.json()['user_photo'] == 'no photo yet'
