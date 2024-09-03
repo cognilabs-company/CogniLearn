d77ae671c6eaedad362c6bb0d9fa7f23d88dc33c
user <user@kali.user>
2024-09-03 21:15:18 +0500

    docker and google login

--- auth/auth.py
+++ auth/auth.py
@@ -1,13 +1,20 @@
 from datetime import timedelta
 from typing import Annotated
+
+import jwt
+import requests
 from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
+
+from config import GOOGLE_CLIENT_SECRET_KEY, GOOGLE_CLIENT_ID, GOOGLE_REDIRECT_URL
+from fg import send_mail_login_password
 from model.model import Users, Roles
-from database import db_dependency
+from database import db_dependency, get_db
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from auth.scheme import CreateUserRequest,Token
-from utils import hash_password,authenticate_user,create_access_token, get_current_user,verify_password
+from utils import hash_password, authenticate_user, create_access_token, get_current_user, verify_password, \
+    generate_token
 from model.model import Users as users
 import re
 
@@ -18,6 +25,7 @@ router = APIRouter(
 )
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
@@ -133,6 +141,81 @@ async def change_password(
     db.commit()
     return {'message': 'Password changed successfully'}
 
+
+@router.get("/login/google")
+async def login_google():
+    return {
+        "url": f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URL}&scope=openid%20profile%20email&access_type=offline"
+    }
+
+
+@router.get("/google")
+async def auth_google(code: str, session:db_dependency):
+    token_url = "https://accounts.google.com/o/oauth2/token"
+    data = {
+        "code": code,
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET_KEY,
+        "redirect_uri": GOOGLE_REDIRECT_URL,
+        "grant_type": "authorization_code",
+    }
+    response = requests.post(token_url, data=data)
+    access_token = response.json().get("access_token")
+    user_info = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
+    user_result = user_info.json()
+    password = re.sub(r'\.', '', re.match(r'^[^@]+', user_result['email']).group())
+    user_data = {
+        'email' : user_result['email'],
+        'first_name' : user_result['name'],
+        'last_name' : user_result['given_name'],
+        'password' : pwd_context.hash(password)
+    }
+    query_exist = session.query(users).filter(users.email == user_data['email']).first()
+    if query_exist:
+        raise HTTPException(status_code=400, detail="Email already in use")
+
+    user_role = session.query(Roles).filter(Roles.role_name == "user").first()
+    if user_role is None:
+        create_role = Roles(
+            role_name="user"
+        )
+        session.add(create_role)
+        session.commit()
+
+    admin_role = session.query(Roles).filter(Roles.role_name == 'admin').first()
+    if admin_role is None:
+        create_admin_role = Roles(
+            role_name="admin"
+        )
+        session.add(create_admin_role)
+        session.commit()
+
+    user_role_query = session.query(Roles).filter(Roles.role_name == "user").first()
+
+    q = Users(
+        email=user_data['email'],
+        username=user_data['email'],
+        name=user_data['first_name'],
+        hashed_password=user_data['password'],
+        role_id=user_role_query.id
+    )
+    query = session.add(q)
+    session.commit()
+
+
+    user_data1 = session.query(users).filter(users.email == user_data["email"]).first()
+
+
+    # token = generate_token(user_data1.id)
+    token = create_access_token(user_data1.email, user_data1.id, timedelta(minutes=20))
+    send_mail_login_password(user_data1.email, password)
+    session.commit()
+    message = {"message": "Check your email for login information"}
+    return message, token
+
+@router.get("/token")
+async def get_token(token: str = Depends(oauth2_bearer)):
+    return jwt.decode(token, GOOGLE_CLIENT_SECRET_KEY, algorithms=["HS256"])
 
 
 
